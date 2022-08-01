f70ec3f15213c6ac59e4144387e8c5fe7eeda1c0
Dimo Todorov <baal98@gmail.com>
2022-08-01 07:27:21 +0300

    Add files via upload

---
+++ Library/LibraryIterator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private  List<Book> books;
+        private int index;
+
+        public LibraryIterator(List<Book> books)
+        {
+            this.books = books;
+            this.index = -1;
+        }
+        public Book Current => books[index];
+
+        object IEnumerator.Current => Current;
+
+       
+        public bool MoveNext()
+        {
+            index++;
+            if (index < books.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+        public void Dispose()
+        {
+        }
+
+    }
+}
