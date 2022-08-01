94959cf3b687f906ca0db49965ef92b4b35484ae
Dimo Todorov <baal98@gmail.com>
2022-08-01 07:26:45 +0300

    Add files via upload

---
+++ ComparableBook/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors.ToList();
+        }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            if (this.Year > other.Year)
+            {
+                return 1;
+            }
+            else if (this.Year < other.Year)
+            {
+                return -1;
+            }
+            else
+            {
+                return this.Title.CompareTo(other.Title);
+
+                //if (this.Title.CompareTo(other.Title) > 0)
+                //{
+                //    return 1;
+                //}
+                //else if (this.Title.CompareTo(other.Title) < 0)
+                //{
+                //    return -1;
+                //}
+                //else
+                //{
+                //    return 0;
+                //}
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}"; 
+        }
+    }
+}
---
+++ ComparableBook/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+
+            Library libraryOne = new Library();
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+           
+
+            foreach (var book in libraryTwo)
+            {
+                Console.WriteLine(book);
+            }
+
+
+            //Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            //Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            //Book bookThree = new Book("The Documents in the Case", 1930);
+            //Book bookFourt = new Book("The EAlive in the Case", 1930);
+
+            //List<Book> books = new List<Book>()
+            //{
+            //    bookOne, bookTwo, bookThree,bookFourt
+            //};
+
+            ////var sortedBooks = books.OrderBy(x => x.Year).ToList();
+            //books.Sort();
+
+            //foreach (var book in books)
+            //{
+            //    Console.WriteLine(book);
+            //}
+        }
+    }
+}
