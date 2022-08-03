db56a88831f682215bb2ba0c3aa8c75e59ffe147
Dimo Todorov <baal98@gmail.com>
2022-08-04 00:34:38 +0300

    Add files via upload

---
+++ Froggy/Lake.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        public List<int> Stones { get; set; }
+        public int Index { get; set; }
+
+        public Lake(List<int> stones)
+        {
+            this.Stones = stones;
+            this.Index = 0;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < Stones.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return Stones[i];
+                }
+            }
+            for (int i = Stones.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return Stones[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        => this.GetEnumerator();
+    }
+}
---
+++ Froggy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            Lake stones = new Lake(inputs);
+            Console.WriteLine(String.Join(", ", stones));
+        }
+    }
+}
