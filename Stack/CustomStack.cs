d30762e68da2adb377aa2473c8be4c7655a52cd8
Dimo Todorov <baal98@gmail.com>
2022-08-04 00:35:43 +0300

    Add files via upload

---
+++ Stack/CustomStack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        public CustomStack()
+        {
+            Elements = new List<T>();
+            Count = 0;
+        }
+        public List<T> Elements { get; set; }
+        public int Count { get; set; }
+
+        public void Pop()
+        {
+            if (Elements.Count == 0)
+            {
+                throw new Exception("No elements");
+            }
+            int index = 0;
+            if (Elements.Count > 1)
+            {
+                index = Elements.Count - 1;
+            }
+
+            Elements.RemoveAt(index);
+            Count--;
+        }
+
+        public void Push(T element)
+        {
+            Count++;
+            Elements.Add(element);
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+
+            for (int i = Elements.Count - 1; i >= 0; i--)
+            {
+                yield return Elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace IteratorsAndComparators
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            string commands;
+
+            try
+            {
+                while ((commands = Console.ReadLine()) != "END")
+                {
+                    string[] splittedCommands = commands.Split(new string[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries);
+
+
+                    if (splittedCommands[0] == "Push")
+                    {
+                        for (int i = 1; i < splittedCommands.Length; i++)
+                        {
+                            stack.Push(int.Parse(splittedCommands[i]));
+                        }
+                    }
+                    else
+                    {
+                        if (stack.Count == 0)
+                        {
+                            Console.WriteLine("No elements");
+                            continue;
+                        }
+                        stack.Pop();
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
